edab130180a07dc4dda8eafec9f3912c34fee088
Unknown <unknown@example.com>
2020-05-26 01:04:59 +0300

    Lesson 13. Сonditions

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+"use strict";
 /* 12. Практика. ч1 Начинаем создавать приложение
 
 Задание на урок:
@@ -39,3 +40,49 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+
+/* Урок 13 - Условия  - Сonditions */
+
+if (4 == 9) {
+    console.log("Ok!");
+} else {
+    console.log("Error");
+}
+
+const num = 50;
+
+if (num < 49) {
+    console.log("Error");
+} else if (num > 100) {
+    console.log("Много");
+} else {
+    console.log("OK!");
+}
+
+/* Тернарный оператор! - в его работе участвует 3 агрумента, 
+это единсвтенный тернарный оператор, который есть в JS  на текущий момент */
+(num === 50) ?  console.log("OK!") : console.log("Error");
+
+/* Бинарный аргумент:
+    4 + 4;
+Унарный аргумент:
+    +'4' */
+
+ /* конструкция switch js  на строгое сравнение! */
+
+switch (num) {
+    case 49:
+        console.log("Неверно!");
+        break;
+    case 100:
+        console.log("Неверно!");
+        break;
+    case 50:
+        console.log("В точку!");
+        break;
+    default:
+        console.log("Не в этот раз!");
+        break; 
+}
+
+
