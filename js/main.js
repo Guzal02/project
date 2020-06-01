6f8d2f827299be3986747856e97bfe7a76e86fe1
Guzal <02guzal@gmail.com>
2020-06-01 17:34:41 +0300

    22. Передача по ссылке или по  значению, Spread оператор (ES6-ES9)

--- js/main.js
+++ js/main.js
@@ -481,10 +481,109 @@ P.S. Функции вызывать не обязательно*/
 // products.sort();
 // console.log(products.join("; "));
 
-const arr = [19, 3, 36, 8, 10];
-arr.sort(compareNum);
-console.log(arr);
+// const arr = [19, 3, 36, 8, 10];
+// arr.sort(compareNum);
+// console.log(arr);
 
-function compareNum(a, b) {
-    return a - b;
+// function compareNum(a, b) {
+//     return a - b;
+// }
+
+// /* 22. Передача по ссылке или по
+// значению, Spread оператор (ES6-ES9) */
+
+// let a = 5,
+//     b = a;
+
+// b = b + a;
+
+// console.log(a);
+// console.log(b);
+
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
+// const copy = obj;
+
+// copy.a = 10;
+
+// console.log(copy); /* { a: 10, b: 1 }  */
+// console.log(obj); /*  { a: 10, b: 1 } */
+
+function copy(mainObj) {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+
+    return objCopy;
 }
+
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+const newNumbers = copy(numbers);
+
+newNumbers.a = 10;
+newNumbers.c.x = 10;
+
+// console.log(newNumbers);
+// console.log(numbers);
+
+const add = {
+    d: 17,
+    e: 20
+};
+
+console.log(Object.assign(numbers, add));
+
+const clone = Object.assign({}, add);
+
+clone.d = 20;
+
+// console.log(add);
+// console.log(clone);
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1] = 'adasdasda';
+console.log(newArray); /* [ 'a', 'adasdasda', 'c' ] */
+console.log(oldArray); /* [ 'a', 'b', 'c' ]  */
+
+/* Оператор разворота -  Spread*/
+
+const video = ['youtube', 'video', 'rutube'],
+      blogs = ['wordpress', 'livejournal', 'blogger'],
+      internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+const num = [2, 5, 7];
+
+log(...num);
+
+const array = ['a', 'b'];
+
+const newAarray = [...array];
+
+const q = {
+    one: 1,
+    two: 2
+}
+
+const newObj = {...q};
