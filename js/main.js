66b3a26e312f8bc2fa996a56f74ec594552e0886
Guzal <02guzal@gmail.com>
2020-06-08 21:49:35 +0300

    26. Динамическая типизация в JS

--- js/main.js
+++ js/main.js
@@ -714,3 +714,60 @@ const personalMovieDB = {
         });
     }
 };
+
+/* 25. Отлавливаем ошибки в своем коде при помощи консоли разработчика. Breakpoints */
+
+/* 26. Динамическая типизация в JS */
+
+// To String
+
+// 1)
+console.log(typeof(String(null))); // string
+console.log(typeof(String(4))); // string
+
+// 2)
+
+console.log(typeof(5 + "")); // string
+console.log(typeof(null + "")); // string
+
+const num = 5;
+
+console.log("https://vk.com/catalog/" + num); // https://vk.com/catalog/5
+
+const fontSize = 26 + 'px';
+
+// To Number
+// 1)
+console.log(typeof(Number('4'))); // number
+// 2)
+console.log(typeof(+'5')); // number
+
+// 3)
+console.log(typeof(parseInt('15px', 10))); // number
+
+
+let answ = +prompt("Hello", "");
+
+// To boolean
+
+// 0, '', null, undefined, NaN; - false!
+
+// 1)
+let switcher = null;
+
+if (switcher) {
+    console.log('Working...'); //  не раб!
+}
+
+let switcher = 1;
+
+if (switcher) {
+    console.log('Working...'); // Working...
+}
+// 2)
+
+console.log(typeof(Boolean('4'))); // boolean
+
+// 3)
+
+console.log(typeof(!!'44444')); // boolean
