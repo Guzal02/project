375012ed329273136a515f232d39aca6ddc535ca
Guzal <02guzal@gmail.com>
2020-06-08 23:21:29 +0300

    27. Задачи с собеседований на понимание основ

--- js/main.js
+++ js/main.js
@@ -771,3 +771,52 @@ console.log(typeof(Boolean('4'))); // boolean
 // 3)
 
 console.log(typeof(!!'44444')); // boolean
+
+
+/* 27. Задачи с собеседований на понимание основ */
+
+// 1) Какое будет выведено значение: let x = 5; alert( x++ ); ?  - 5!
+
+// 2) Чему равно такое выражение: [ ] + false - null + true ? 
+
+console.log([] + false); // - "false" - string
+console.log([] + false - null); // - "NaN"
+console.log([] + false - null -true); // "NaN"
+ 
+ // 3) Что выведет этот код: let y = 1; let x = y = 2; alert(x); ? - 2
+
+// 4) Чему равна сумма [ ] + 1 + 2?   
+
+console.log([] + 1 + 2); // "12"
+
+
+// 5) Что выведет этот код: alert( "1"[0] )? - 1
+
+// 6)  Чему равно 2 && 1 && null && 0 && undefined ?
+
+console.log(2 && 1 && null && 0 && undefined); // null  -- && запинается на лжи!
+
+// 7) Есть ли разница между выражениями? !!( a && b ) и (a && b)? - да!!!! 
+
+
+// 8) Что выведет этот код: alert( null || 2 && 3 || 4 ); ?  //  || запинается на правде  !  ответ 3
+
+console.log(null || 2 && 3 || 4 );   
+
+// 9) a = [1, 2, 3]; b = [1, 2, 3]; Правда ли что a == b ?  -- нет !
+    const a = [1, 2, 3]; 
+    const b = [1, 2, 3];
+
+    console.log(a == b); // false
+
+// 10) Что выведет этот код: alert( +"Infinity" ); ? - Infinity
+
+
+// 11) Верно ли сравнение: "Ёжик" > "яблоко"  - false
+
+// 12)  Чему равно 0 || "" || 2 || undefined || true || falsе ?
+
+console.log(0 || "" || 2 || undefined || true || falsе);  // 2
+
+ 
+ 
